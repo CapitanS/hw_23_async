200c44b18fa321cd7c99023f1d0efa41b9f5fce7
dimat <dimaprizma2016@gmail.com>
2021-03-29 20:26:49 +0300

    - used .gather() in main_functions.py

--- main_functions.py
+++ main_functions.py
@@ -14,7 +14,7 @@ async def openweather_func():
     loop = asyncio.get_event_loop()
 
     request_openweather = loop.run_in_executor(None, requests.get,
-                                               f'https://api.openweathermap.org/data/2.5/onecall?lat=49.9901&lon=36.2303&exclude=hourlys&appid=d{openweather_api_key}8&units=metric')
+                                               f'https://api.openweathermap.org/data/2.5/onecall?lat=49.9901&lon=36.2303&exclude=hourlys&appid={openweather_api_key}&units=metric')
 
     openweather = await request_openweather
 
@@ -49,11 +49,16 @@ async def metaweather_func():
     temperature_kharkov.append(json_metaweather["consolidated_weather"][0]["the_temp"])
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(openweather_func())
-loop.run_until_complete(weatherstack_func())
-loop.run_until_complete(metaweather_func())
+async def main():
+    # Schedule three calls *concurrently*:
+    await asyncio.gather(
+        openweather_func(),
+        weatherstack_func(),
+        metaweather_func(),
+    )
 
-avg_temperature_kharkov = mean(temperature_kharkov)
+    avg_temperature_kharkov = mean(temperature_kharkov)
 
-print("The temperature of the Kharkov =", round(avg_temperature_kharkov, 2), u'\u2103')
+    print("The temperature of the Kharkov =", round(avg_temperature_kharkov, 2), u'\u2103')
+
+asyncio.run(main())
